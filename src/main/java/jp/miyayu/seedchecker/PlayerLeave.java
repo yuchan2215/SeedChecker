ed13715ff6a051dfe5f0f646781fc1fb973e9bac
yuchan2215 <38094366+yuchan2215@users.noreply.github.com>
2020-06-25 10:37:38 +0900

    プレイヤーの退出時の処理を改善

--- src/main/java/jp/miyayu/seedchecker/PlayerLeave.java
+++ src/main/java/jp/miyayu/seedchecker/PlayerLeave.java
@@ -1,9 +1,12 @@
 package jp.miyayu.seedchecker;
 
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -12,26 +15,42 @@ import java.sql.Statement;
 public class PlayerLeave implements Listener {
     @EventHandler
     public void onPlayerQuitEvent(PlayerQuitEvent event){
-        try(Connection connection = DataBaseUtil.getSQLConnection()){
-            assert connection != null;
-            Statement statement = connection.createStatement();
-            String uuid = event.getPlayer().getUniqueId().toString();
-            String seed = event.getPlayer().getLocation().getWorld().getSeed() + "";
-            ResultSet resultSet = statement.executeQuery("SELECT * from users WHERE uuid = '"+uuid+"'");
-            if(resultSet.next()){
-                statement.close();
-                statement = connection.createStatement();
-                statement.executeUpdate("UPDATE users SET leaveseed = '"+seed+"' WHERE uuid = '"+uuid+"'");
-                statement.close();
-            }else{
-                statement.close();
-                statement = connection.createStatement();
-                statement.executeUpdate("INSERT INTO users (uuid,leaveseed) VALUES ('"+uuid+"','"+seed+"')");
-                statement.close();
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                playerQuit(event.getPlayer(),false);
             }
-        }catch(Exception e){
-            e.printStackTrace();
-            BroadcastUtils.broadcastMessage(ChatColor.GOLD  + "[SeedChecker]" + ChatColor.RED + "データベースへの接続に失敗しました");
-        }
+        }.runTaskAsynchronously(MainClass.instance);
     }
+
+    public static void playerQuit(Player player,boolean isFinish){
+                try(Connection connection = DataBaseUtil.getSQLConnection()){
+                    assert connection != null;
+                    Statement statement = connection.createStatement();
+                    String uuid = player.getUniqueId().toString();
+                    String seed = player.getLocation().getWorld().getSeed() + "";
+                    ResultSet resultSet = statement.executeQuery("SELECT * from users WHERE uuid = '"+uuid+"'");
+                    if(resultSet.next()){
+                        statement.close();
+
+
+
+
+                        statement = connection.createStatement();
+                        statement.executeUpdate("UPDATE users SET leaveseed = '"+seed+"' WHERE uuid = '"+uuid+"'");
+                        statement.close();
+                    }else{
+                        statement.close();
+                        statement = connection.createStatement();
+                        statement.executeUpdate("INSERT INTO users (uuid,leaveseed) VALUES ('"+uuid+"','"+seed+"')");
+                        statement.close();
+                    }
+                    if(isFinish)
+                        Bukkit.getLogger().info(player.getName() + "の退出処理を行いました");
+                }catch(Exception e){
+                    e.printStackTrace();
+                    BroadcastUtils.broadcastMessage(ChatColor.GOLD  + "[SeedChecker]" + ChatColor.RED + "データベースへの接続に失敗しました");
+                }
+    }
+
 }
