c2b044cab83d76295d4cc10de12a5c221d9f39e4
yuchan2215 <38094366+yuchan2215@users.noreply.github.com>
2020-06-25 10:38:16 +0900

    Join時の処理を非同期にした

--- src/main/java/jp/miyayu/seedchecker/PlayerJoin.java
+++ src/main/java/jp/miyayu/seedchecker/PlayerJoin.java
@@ -7,6 +7,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.sql.Connection;
 import java.sql.ResultSet;
@@ -15,33 +16,39 @@ import java.sql.Statement;
 public class PlayerJoin implements Listener {
     @EventHandler
     public void onPlayerJoinEvent(PlayerJoinEvent event){
-        try(Connection connection = DataBaseUtil.getSQLConnection()){
-            assert connection != null;
-            Statement statement = connection.createStatement();
-            String uuid = event.getPlayer().getUniqueId().toString();
-            ResultSet resultSet = statement.executeQuery("SELECT * from users WHERE uuid = '"+uuid+"'");
-            if(resultSet.next()){
-                long leaveSeed = resultSet.getLong("leaveseed");
-                long joinSeed = event.getPlayer().getLocation().getWorld().getSeed();
-                if(leaveSeed != joinSeed){
-                    event.getPlayer().sendMessage(ChatColor.AQUA + "ログアウトしたワールドにリセットがかかっていたため、メインワールドにリスポーンしました");
-                    Location oldLocation = event.getPlayer().getLocation();
-                    String world = oldLocation.getWorld().getName();
-                    String x = oldLocation.getBlockX()+"";
-                    String y = oldLocation.getBlockY()+"";
-                    String z = oldLocation.getBlockZ()+"";
-                    String name = event.getPlayer().getName();
-                    BroadcastUtils.broadcastMessage(String.format(ChatColor.GRAY + "%sは%s(%s,%s,%s)でログアウトしましたが、シード値の変更を検知したため初期スポーン地点に転送されました",
-                            name,world,x,y,z));
-                    Location spawn = Bukkit.getWorld("world").getSpawnLocation();
-                    event.getPlayer().teleport(spawn);
-                    event.getPlayer().setFireTicks(0);
-                }
+        new BukkitRunnable(){
+            @Override
+            public void run() {
+                try(Connection connection = DataBaseUtil.getSQLConnection()){
+                    assert connection != null;
+                    Statement statement = connection.createStatement();
+                    String uuid = event.getPlayer().getUniqueId().toString();
+                    ResultSet resultSet = statement.executeQuery("SELECT * from users WHERE uuid = '"+uuid+"'");
+                    if(resultSet.next()){
+                        long leaveSeed = resultSet.getLong("leaveseed");
+                        long joinSeed = event.getPlayer().getLocation().getWorld().getSeed();
+                        if(leaveSeed != joinSeed){
+                            event.getPlayer().sendMessage(ChatColor.AQUA + "ログアウトしたワールドにリセットがかかっていたため、メインワールドにリスポーンしました");
+                            Location oldLocation = event.getPlayer().getLocation();
+                            String world = oldLocation.getWorld().getName();
+                            String x = oldLocation.getBlockX()+"";
+                            String y = oldLocation.getBlockY()+"";
+                            String z = oldLocation.getBlockZ()+"";
+                            String name = event.getPlayer().getName();
+                            BroadcastUtils.broadcastMessage(String.format(ChatColor.GRAY + "%sは%s(%s,%s,%s)でログアウトしましたが、シード値の変更を検知したため初期スポーン地点に転送されました",
+                                    name,world,x,y,z));
+                            Location spawn = Bukkit.getWorld("world").getSpawnLocation();
+                            event.getPlayer().teleport(spawn);
+                            event.getPlayer().setFireTicks(0);
+                        }
 
+                    }
+                }catch(Exception e){
+                    e.printStackTrace();
+                    BroadcastUtils.broadcastMessage(ChatColor.GOLD  + "[SeedChecker]" + ChatColor.RED + "データベースへの接続に失敗しました");
+                }
             }
-        }catch(Exception e){
-            e.printStackTrace();
-            BroadcastUtils.broadcastMessage(ChatColor.GOLD  + "[SeedChecker]" + ChatColor.RED + "データベースへの接続に失敗しました");
-        }
+        }.runTaskAsynchronously(MainClass.instance);
+
     }
 }
